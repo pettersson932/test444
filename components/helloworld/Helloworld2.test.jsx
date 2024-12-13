4ad546d726c0a8e4a8620c884ad5a680adc0a614
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2024-12-13 14:40:29 +0100

    x

--- components/helloworld/Helloworld2.test.jsx
+++ components/helloworld/Helloworld2.test.jsx
@@ -4,6 +4,6 @@ import HelloWorld2 from "./Helloworld2";
 
 test("renders greeting message with the provided name", () => {
   render(<HelloWorld2 name="John" />);
-  const heading = screen.getByRole("heading", { name: /Hello, John4!/i });
+  const heading = screen.getByRole("heading", { name: /Hello, John!/i });
   expect(heading).toBeInTheDocument(); // Using jest-dom matcher
 });
